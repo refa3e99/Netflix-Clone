9268765e44be92f52474038fd6aba9145228ea4c
refa3e99 <refa3e99@icloud.com>
2022-05-18 01:37:38 +0300

    modal added

--- ntflx/src/components/Card.js
+++ ntflx/src/components/Card.js
@@ -1,19 +1,49 @@
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import  Modal from "react-bootstrap/Modal";
+import { useState } from "react";
+import ModalMovie from "./ModalMovie/ModalMovie";
 export default function CardMovie(props){
-  let imgUrl = "https://image.tmdb.org/t/p/original";
+  const [show,setShow] = useState(false);
+  const [choosenCard,setChoosenCard] = useState();
+
+  const handleShow = (movie) => {
+    setShow(true);
+    setChoosenCard(movie);
+  }
+  const handleClose = () => setShow(false);
+  
     return (
         <>
-<Card style={{ width: '18rem' }}>
+<Card style={{ width: "18rem",
+            textAlign: "center",
+            marginTop: "3rem",
+            boxShadow:
+            "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",}}>
 <Card.Img variant="top" src={`https://image.tmdb.org/t/p/original${props.movie.poster_path}`} />
   <Card.Body>
     <Card.Title>{props.movie.title}</Card.Title>
     <Card.Text>
       {props.movie.overview}
     </Card.Text>
-    <Button variant="primary">Add to favorites</Button>
+    <Button variant="primary"
+    onClick={() => {
+      handleShow(props.movie);
+    }}
+    >Add to favorites</Button>
   </Card.Body>
 </Card>
+{
+  choosenCard && (
+    <ModalMovie
+      show={show}
+      handleClose={handleClose}
+      choosenCard={choosenCard}
+      poster_path={props.movie.poster_path}
+      title={props.movie.title}
+    />
+  )
+}
         </>
     )
 }
--- ntflx/src/components/ModalMovie/ModalMovie.js
+++ ntflx/src/components/ModalMovie/ModalMovie.js
@@ -1,6 +1,52 @@
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useRef } from "react";
+
 function ModalMovie(props) {
     return (<>
+        <Modal show={props.show} onHide={props.handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{props.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img
+            src={`https://image.tmdb.org/t/p/original${props.poster_path}`}
+            alt="recipe"
+            style={{ width: "100%" }}
+          />
+          <br />
+          
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Add Comment</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Comment"
+              />
+              <Form.Text className="text-muted">add your own comment</Form.Text>
+            </Form.Group>
 
+            <Button
+              variant="primary"
+              type="submit"
+            >
+              Submit comment
+            </Button>
+            <Button
+              variant="primary"
+              type="submit"
+            >
+              Add to favorite
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={props.handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
 </>)
 }
 export default ModalMovie;
